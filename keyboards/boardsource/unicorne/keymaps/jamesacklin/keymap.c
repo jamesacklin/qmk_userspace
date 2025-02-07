fed2a4f783607822233bdb588a2a5da0adf78a32
James Acklin <james.acklin@gmail.com>
2025-02-07 11:37:38 -0500

    move layer modifiers around

--- keyboards/boardsource/unicorne/keymaps/jamesacklin/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/jamesacklin/keymap.c
@@ -65,14 +65,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_split_3x6_3( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_ESC  ,
+       KC_LALT,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_ESC  ,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       OSM_CTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                     KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,KC_QUOT ,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
- OSM(MOD_LSFT),   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                     KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,OSL_FUN ,
+ OSM(MOD_LSFT),   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                     KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_TAB ,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LALT, KC_LGUI, LOW_BSP,    RSE_ENT,  KC_SPC,  OSM_SFT
-                                      //`--------------------------'  `--------------------------'
+                                       MO(_LOWER), KC_LGUI, KC_BSPC,   KC_ENT  ,KC_SPC  , MO(_RAISE)
+                                    //`----------------------------'  `-----------------------------'
   ),
 
 [_LOWER] = LAYOUT_split_3x6_3( \
