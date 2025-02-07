5fc2046d5ad4c594f7c9d17ed4e604aa949f65f1
James Acklin <james.acklin@gmail.com>
2025-02-07 14:32:30 -0500

    alt/tab mod key

--- keyboards/boardsource/unicorne/keymaps/jamesacklin/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/jamesacklin/keymap.c
@@ -57,6 +57,7 @@ combo_t key_combos[COMBO_COUNT] = {
 #define OSM_SFT  OSM(MOD_LSFT)
 #define LOW_BSP  LT(_LOWER, KC_BSPC)
 #define RSE_ENT  LT(_RAISE, KC_ENT)
+#define MOD_TAB  MT(MOD_LALT, KC_TAB)
 
 // For _RAISE layer
 #define CTL_ESC  LCTL_T(KC_ESC)
@@ -65,11 +66,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_split_3x6_3( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_LALT,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_ESC  ,
+       MOD_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_ESC  ,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       OSM_CTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                     KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,KC_QUOT ,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
- OSM(MOD_LSFT),   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                     KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_TAB ,
+ OSM(MOD_LSFT),   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                     KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,OSL_FUN ,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                        MO(_LOWER), KC_LGUI, KC_BSPC,   KC_ENT  ,KC_SPC  , MO(_RAISE)
                                     //`----------------------------'  `-----------------------------'
